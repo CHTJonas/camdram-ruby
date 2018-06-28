a2556650da1828070947d39d758b796371628e3a
Charlie Jonas <devel@charliejonas.co.uk>
2018-06-28 15:47:28 +0100

    Refactor instance variable code

--- lib/camdram/base.rb
+++ lib/camdram/base.rb
@@ -9,11 +9,7 @@ module Camdram
     # @param options [Hash] A single JSON hash with symbolized keys
     # @return [Base]
     def initialize(options = {})
-      options.each do |key, value|
-        # Only set the instance variable if the class or sub-class
-        # has an associated attr_accessor for that variable
-        instance_variable_set("@#{key}", value) if self.respond_to?(key)
-      end
+      set_from_hash(options)
     end
 
     # Update the object from it's unqiue Camdram URL
@@ -24,8 +20,21 @@ module Camdram
       uri = URI( url )
       response = HTTP.get(uri, 3)
       json = JSON.parse(response, symbolize_names: true)
-      self.send(:initialize, json)
+      set_from_hash(json)
       return self
+    end
+
+    private
+
+    # Sets the object's instance variables from a JSON hash
+    #
+    # @param options [Hash] A single JSON hash with symbolized keys.
+    def set_from_hash(options)
+      options.each do |key, value|
+        # Only set the instance variable if the class or sub-class
+        # has an associated attr_accessor for that variable
+        instance_variable_set("@#{key}", value) if self.respond_to?(key)
+      end
     end
   end
 end
