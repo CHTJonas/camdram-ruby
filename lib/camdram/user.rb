d6fd84450a7d67993e11f0678872cb3157c8fef8
Charlie Jonas <devel@charliejonas.co.uk>
2018-10-20 14:30:51 +0100

    Adds functionality to access user's venues

--- lib/camdram/user.rb
+++ lib/camdram/user.rb
@@ -36,10 +36,36 @@ module Camdram
 
     # Return an array of societies the user is authorised for
     #
-    # @return [Array] An array of Camdram::Organisation objects
+    # @return [Array] An array of Camdram::Organisation objects.
     def get_orgs
+      orgs_share('society')
+    end
+
+    # Return an array of venues the user is authorised for
+    #
+    # @return [Array] An array of Camdram::Venue objects.
+    def get_venues
+      orgs_share('venue')
+    end
+
+    private
+
+    # Shared code because Camdram stores venues and societies together in the same table
+    #
+    # @param orm_type [String] The ORM type to match against (either "society" or "venue").
+    # @return [Array] An array of objects of the specified ORM type's class.
+    def orgs_share(orm_type)
+      object = case orm_type
+        when 'society' then Organisation
+        when 'venue' then Venue
+      end
       slug = "/auth/account/organisations.json"
-      get_array(slug, Organisation)
+      json = get(slug)
+      objects = Array.new
+      json.each do |obj|
+        objects << object.new( obj, @http ) if obj[:_type] == orm_type
+      end
+      return objects
     end
 
   end
